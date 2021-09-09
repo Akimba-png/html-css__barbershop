6cf9fbf98c39de4baa41441017b83baf3ce7a685
Akimba-png <akimba@gmail.com>
2021-09-09 16:39:28 +0300

    Настраивает окружение. Начальное состояние проекта

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+const styles = () => {
+  return gulp.src('./source/css/**/*.css')
+  .pipe(browserSync.stream());
+};
+
+exports.styles = styles;
+
+const server = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: './source',
+    },
+  });
+  done();
+};
+
+const watcher = () => {
+  gulp.watch('./source/css/**/*css', styles)
+  gulp.watch('./source/*.html').on('change', browserSync.reload);
+};
+
+exports.default = gulp.series(server, watcher);
