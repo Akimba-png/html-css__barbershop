8fd7c9e7504bcc5b791e17d9c831820a2b008f12
Akimba-png <akimba@gmail.com>
2021-11-04 17:26:25 +0300

    Добавляет стилизацию и сетку основных блоков главной страницы. Добавляет SVG-спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
 
 const styles = () => {
   return gulp.src('./source/css/**/*.css')
   .pipe(browserSync.stream());
 };
-
 exports.styles = styles;
+
+const createSprite = () => {
+  return gulp.src('./source/img/icons/*.svg')
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: "../sprite.svg",
+      }
+    }
+  }))
+  .pipe(gulp.dest('./source/img/icons'))
+}
+exports.createSprite = createSprite;
+
 
 const server = (done) => {
   browserSync.init({
