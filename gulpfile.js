b111ba02dcf9c7e09a595e2302f2190e1a274707
Akimba-png <akimba@gmail.com>
2021-12-22 20:58:57 +0300

    Добавляет сборку приложения на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,53 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const del = require('del');
+const rename = require('gulp-rename');
+const htmlMin = require('gulp-htmlmin');
+const postCss = require('gulp-postcss');
+const sourcemap = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const cleanСss = require('gulp-clean-css');
 const svgSprite = require('gulp-svg-sprite');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+
+
+const clean = () => {
+  return del('build');
+};
+
+const minifyHtml = () => {
+  return gulp.src('./source/*.html')
+    .pipe(htmlMin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./build'));
+};
 
 const styles = () => {
-  return gulp.src('./source/css/**/*.css')
+  return gulp.src('./source/css/style.css')
+    .pipe(sourcemap.init())
+    .pipe(cleanСss())
+    .pipe(postCss([
+      autoprefixer()
+    ]))
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('./build/css'))
   .pipe(browserSync.stream());
 };
-exports.styles = styles;
+
+const copy = () => {
+  return gulp.src([
+    './source/fonts/*.{woff2,woff}',
+    './source/img/**/*.{png,jpg,svg}'
+  ],
+  {
+    base: 'source'
+  })
+    .pipe(gulp.dest('./build'))
+};
 
 const createSprite = () => {
-  return gulp.src('./source/img/icons/*.svg')
+  return gulp.src('./build/img/icons/*.svg')
   .pipe(svgSprite({
     mode: {
       stack: {
@@ -17,15 +55,29 @@ const createSprite = () => {
       }
     }
   }))
-  .pipe(gulp.dest('./source/img/icons'))
+  .pipe(gulp.dest('./build/img/icons'))
 }
-exports.createSprite = createSprite;
 
+const compressImages = () => {
+  return gulp.src('./build/img/**/*.{png,jpg,svg}')
+    .pipe(imagemin([
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('./build/img'))
+};
+
+const createWebp = () => {
+  return gulp.src('./build/img/**/*.{jpg,png}')
+    .pipe(webp({ quality: 50 }))
+    .pipe(gulp.dest('./build/img'))
+};
 
 const server = (done) => {
   browserSync.init({
     server: {
-      baseDir: './source',
+      baseDir: './build',
     },
   });
   done();
@@ -33,7 +85,30 @@ const server = (done) => {
 
 const watcher = () => {
   gulp.watch('./source/css/**/*css', styles)
-  gulp.watch('./source/*.html').on('change', browserSync.reload);
+  gulp.watch('./source/*.html', minifyHtml).on('change', browserSync.reload);
 };
 
-exports.default = gulp.series(server, watcher);
+exports.default = gulp.series(
+  clean,
+  copy,
+  gulp.parallel(
+    createWebp,
+    createSprite,
+    minifyHtml,
+    styles
+  ),
+  server,
+  watcher
+);
+
+exports.build = gulp.series(
+  clean,
+  copy,
+  compressImages,
+  gulp.parallel(
+    createWebp,
+    createSprite,
+    minifyHtml,
+    styles
+  )
+);
